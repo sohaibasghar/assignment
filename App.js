5401ff616cb22ae8ac377be73f1dcbe79f2ba119
sohaib1997 <sohaibasghar1997@gmail.com>
2020-10-11 10:45:36 +0500

    base project created

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>
+        Hello World
+        </Text>
+      </SafeAreaView>
+    </>
+  );
+};
+export default App;
