56971b602b00b45ef22379eea7267b96601553e6
Sohaib Asghar <arsalan@devigital.com>
2020-12-28 18:28:05 +0500

    adding first commit

--- App.js
+++ App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {
   SafeAreaView,
   Text,
-  StatusBar,
 } from 'react-native';
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>
+    <SafeAreaView>
+      <Text>
         Hello World
         </Text>
-      </SafeAreaView>
-    </>
+    </SafeAreaView>
   );
 };
 export default App;
